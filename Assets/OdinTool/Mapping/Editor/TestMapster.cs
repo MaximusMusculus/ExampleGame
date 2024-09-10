793cfe87911aa0888cdbf93b9c7888e0fb57b899
Max <android.avenger@gmail.com>
2024-09-10 15:57:09 +0300

    маппинг коллекций с полиморфными классами

--- Assets/OdinTool/Mapping/Editor/TestMapster.cs
+++ Assets/OdinTool/Mapping/Editor/TestMapster.cs
@@ -59,14 +59,51 @@ namespace TesMapping
                 Assert.AreEqual(sourceCollection.Collection[i].Count, targetCollection.Collection[i].Count);
             }
         }
+        
+        [Test]
+        public void TestPolymorphicCollectionMapping()
+        {
+            // Arrange
+            var sourceCollection = new SourceCollection
+            {
+                Collection = new List<SourceClass>
+                {
+                    new SourceClass { Name = "Item1", Count = 1 },
+                    new SourceClass { Name = "Item2", Count = 2 },
+                    new SourceClassExtension { Name = "ItemSource", Count = 3, Ext = 100 }
+                }
+            };
 
+            // Act
+            var targetCollection = sourceCollection.Adapt<TargetCollection>(_config);
+
+            // Assert
+            Assert.AreEqual(sourceCollection.Collection.Count, targetCollection.Collection.Count);
+            for (int i = 0; i < sourceCollection.Collection.Count; i++)
+            {
+                Assert.AreEqual(sourceCollection.Collection[i].Name, targetCollection.Collection[i].Name);
+                Assert.AreEqual(sourceCollection.Collection[i].Count, targetCollection.Collection[i].Count);
+
+                // Проверяем полиморфные свойства
+                if (sourceCollection.Collection[i] is SourceClassExtension sourceExt)
+                {
+                    var targetExt = targetCollection.Collection[i] as TargetClassExtension;
+                    Assert.IsNotNull(targetExt);
+                    Assert.AreEqual(sourceExt.Ext, targetExt.Ext);
+                }
+                else
+                {
+                    Assert.IsInstanceOf<TargetClass>(targetCollection.Collection[i]);
+                }
+            }
+        }
     }
 
     public class RegisterMapper : IRegister
     {
         public void Register(TypeAdapterConfig config)
         {
-            config.NewConfig<SourceClass, TargetClass>().RequireDestinationMemberSource(true);
+            config.NewConfig<SourceClass, TargetClass>().RequireDestinationMemberSource(true).Include<SourceClassExtension, TargetClassExtension>();
             config.NewConfig<SourceCollection, TargetCollection>().RequireDestinationMemberSource(true);
         }
     }
@@ -87,10 +124,20 @@ namespace TesMapping
         public int Count;
     }
 
+    public class SourceClassExtension : SourceClass
+    {
+        public int Ext;
+    }
+
     public class TargetClass
     {
         public string Name;
         public int Count;
+    }
+
+    public class TargetClassExtension : TargetClass
+    {
+        public int Ext;
     }
 
 }
---
+++ Assets/OdinTool/Mapping/Meta/MapperMetaConfig.cs
@@ -0,0 +1,9 @@
+using Mapster;
+
+
+namespace OdinTool.Mapping.Meta
+{
+    public class MapperMetaConfig 
+    {
+    }
+}
