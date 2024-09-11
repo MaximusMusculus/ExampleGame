f0419c536f721dca7bf68c8e5a87b60d3a263b0a
Max <android.avenger@gmail.com>
2024-09-11 15:22:52 +0300

    тесты для инвентаря

--- Assets/Scripts/Meta/Configs/MetaConfig.cs
+++ Assets/Scripts/Meta/Configs/MetaConfig.cs
@@ -8,10 +8,9 @@ namespace Meta.Configs
         public int ConfigVersion;
         //данные, что показывет интерфейс игрока (например счетчик ресурсов) тоже формируются и модифицируются в конфиге...
         public List<ItemConfig> InventoryItems = new List<ItemConfig>();
-        
         public List<UnitConfig> Units = new List<UnitConfig>();
     }
-
+    
     
     public class ItemConfig
     {
